de26354ba753a57a676e268b9e40c782d431f29a
Indu Katta <vindukatta@gmail.com>
2019-08-27 21:22:17 +0100

    New changes

--- src/com/factory/Driver.java
+++ src/com/factory/Driver.java
@@ -1,9 +1,14 @@
 package com.factory;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import io.appium.java_client.remote.MobileCapabilityType;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.PageFactory;
 import pageobjects.ShoppingListPage;
@@ -14,6 +19,7 @@ import java.net.URL;
 public class Driver {
 
     public static AppiumDriver driver;
+	
 
     public ShoppingListPage shoppingListPage;
     
@@ -21,12 +27,13 @@ public class Driver {
     	
     }
 
-    public void createDriver(){
+    @SuppressWarnings("rawtypes")
+	public void createDriver(){
         try {
-
-           // String APP = "C:\\Users\\Indu Katta\\AndroidStudioProjects\\shoppinglist-master\\ShoppingList\\build\\outputs\\apk\\play\\debug\\org.openintents.shopping-2.2.0-play-debug.apk";
-        	String APP = "C:\\Users\\Indu Katta\\AndroidStudioProjects\\shoppinglist-master\\ShoppingList\\play\\release\\org.openintents.shopping-2.2.0-play-release.apk";
-            DesiredCapabilities caps = new DesiredCapabilities();
+   
+        	String APP ="C:\\Users\\Indu Katta\\AndroidStudioProjects\\shoppinglist-master\\ShoppingList\\tstore\\release\\org.openintents.shopping-2.2.0-tstore-release.apk";
+        	
+        	DesiredCapabilities caps = new DesiredCapabilities();
 
             caps.setCapability("platformName", "Android");
             caps.setCapability("platformVersion","9.0");
@@ -35,10 +42,11 @@ public class Driver {
             caps.setCapability("app", APP);
             caps.setCapability("avd","Nexus_5X_API_29_x86");
             caps.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 60);
-
+            caps.setCapability(MobileCapabilityType.FULL_RESET, false);
+            caps.setCapability(MobileCapabilityType.NO_RESET, true);
 
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
-
+         
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
--- src/stepdefinitions/ShoppingCartStepActivities.java
+++ src/stepdefinitions/ShoppingCartStepActivities.java
@@ -1,11 +1,19 @@
 package stepdefinitions;
 
 import java.util.List;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+
 import com.factory.Driver;
 import cucumber.api.DataTable;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import io.appium.java_client.android.AndroidKeyCode;
+import io.appium.java_client.android.AndroidKeyMetastate;
 import pageobjects.ShoppingListPage;
 
 public class ShoppingCartStepActivities extends Driver {
